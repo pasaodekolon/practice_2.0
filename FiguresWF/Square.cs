0a929571326d074524149736d5f08f761122e7b1
pasaodekolon <mlololoshka31@gmail.com>
2024-06-11 11:14:43 +0300

    Создал класс square
    Создал класс для расчета квадрата

---
+++ FiguresWF/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresWF
+{
+    internal class square : Figure
+    {
+        public double Side { get; set; }
+
+        public square(double side)
+        {
+            Side = side;
+        }
+
+        public override double square()
+        {
+            return Side * Side;
+        }
+
+        public override double Perimetr
+        {
+            get { return 4 * Side; }
+        }
+
+        public override string ToString()
+        {
+            return $"Квадрат (Сторона = {Side})";
+        }
+    }
+
+}
