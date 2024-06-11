392cfbba0bed5b958c12ca5687184523f54ad555
pasaodekolon <mlololoshka31@gmail.com>
2024-06-11 11:11:42 +0300

    Добавил расчет круга
    Добавил код для расчета окружности

---
+++ FiguresWF/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresWF
+{
+    internal class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Square()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double Perimetr
+        {
+            get { return 2 * Math.PI * Radius; }
+        }
+
+        public override string ToString()
+        {
+            return $"Круг (Радиус = {Radius})";
+        }
+    }
+
+}
